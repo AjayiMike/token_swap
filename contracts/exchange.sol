5c72e6ae17d077f78ece185a232f293d8be3f13c
"AjayiMike" <"ajayimikeofficial@gmail.com">
2022-01-09 22:16:54 +0100

    add emergency withdrawal and buy flag to exchange contract

--- contracts/exchange.sol
+++ contracts/exchange.sol
@@ -6,12 +6,14 @@ contract Exchange {
     IERC20 tokenB;
     uint256 public exchangeRate;
     address public owner;
+    bool buy;
 
     constructor(address tokeAAddress_, address tokenBAddress_, uint256 exchangeRate_) {
         tokenA = IERC20(tokeAAddress_);
         tokenB = IERC20(tokenBAddress_);
         exchangeRate = exchangeRate_;
         owner = msg.sender;
+        buy = true;
     }
 
     modifier onlyOwner {
@@ -23,11 +25,16 @@ contract Exchange {
         // we first check to see if this contract have enough amount of tokenB be to give user for their tokenA
         uint256 thisContractTokenBBalance = tokenB.balanceOf(address(this));
         require(thisContractTokenBBalance >= (_amount * exchangeRate), "No enough tokenB to swap for this amount of tokeA");
+        require(buy == true, "No Active sales");
 
         //this will only work if user have enough amount of tokenA and has granted this contract enough allowance to complete the swap
         tokenA.transferFrom(msg.sender, address(this), _amount);
         //transfering tokenB to user using the exchange rate
         tokenB.transfer(msg.sender, _amount * exchangeRate);
+    }
+
+    function setBuy(bool _setBuy) external onlyOwner {
+        buy = _setBuy;
     }
 
     function changeExchangeRate(uint256 _newRate) public onlyOwner {
@@ -36,5 +43,9 @@ contract Exchange {
 
     function changeOwner(address _newOwner) public onlyOwner {
         owner = _newOwner;
+    }
+
+    function emmergencyWithdrawal(address _token, uint256 _amount) external onlyOwner{
+        IERC20(_token).transfer(msg.sender, _amount);
     }
 }
